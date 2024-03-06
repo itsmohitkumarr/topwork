f03b1cb1bfba03459bf2bfdef7efcce1ea6b2c46
Mohit <its.mohit.kumarr@gmail.com>
2024-03-06 21:04:16 +0530

    topwork logo issue fixing attempt 2

--- components/logo.tsx
+++ components/logo.tsx
@@ -5,7 +5,7 @@ export const Logo = () => {
   return (
     <Link href="/">
       <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
-        <Image src="/logo.png" alt="Logo" height={80} width={120} />
+        <Image src="/logo.svg" alt="Logo" height={80} width={120} />
       </div>
     </Link>
   );
